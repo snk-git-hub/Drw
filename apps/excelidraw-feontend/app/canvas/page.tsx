c4e83f0a854eb7031459e7892a2f914a85f670a3
Shivanandu Dev <shivanandu.dev@gmail.com>
2025-07-24 22:40:15 +0530

    canvas developed ) ;

--- apps/excelidraw-feontend/app/canvas/page.tsx
+++ apps/excelidraw-feontend/app/canvas/page.tsx
@@ -1,61 +1,16 @@
 "use client"
 import { useEffect, useRef } from "react"
+import {initDraw} from "@/app/draw/index";
+
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
      
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-             
-      if (!ctx) return;
-            
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      let clicked=false;
-      let StartX=0;
-      let StartY=0;
-      
-      const getMousePos = (e) => {
-        const rect = canvas.getBoundingClientRect();
-        return {
-          x: e.clientX - rect.left,
-          y: e.clientY - rect.top
-        };
-      };
-      
-      canvas.addEventListener("mousedown",(e)=>{
-        clicked=true;
-        const pos = getMousePos(e);
-        StartX=pos.x;
-        StartY=pos.y;
-      })
-         
-      canvas.addEventListener("mouseup",(e)=>{
-        clicked=false;
-        const pos = getMousePos(e);
-        StartX=pos.x;
-        StartY=pos.y;
-      })
-      
-      canvas.addEventListener("mousemove",(e)=>{
-        if(clicked){
-          const pos = getMousePos(e);
-          const width = pos.x - StartX;
-          const height = pos.y - StartY;
-          ctx.clearRect(0,0,canvas.width,canvas.height);
-          ctx.fillStyle = 'black';
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeStyle= 'white';
-          ctx.strokeRect(StartX,StartY,width,height);
-           
-          console.log(pos.x)
-          console.log(pos.y)
-        }
-      })
-           
+    if(canvasRef.current){
+      initDraw(canvasRef.current);
     }
+    
   }, [canvasRef]);
      
   return (
